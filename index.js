822cda6736a2a7772d569393398092164b155535
Roman Shtylman <shtylman@gmail.com>
2011-11-04 01:32:32 -0400

    expose compile method to directly create templates and render them

--- index.js
+++ index.js
@@ -11,6 +11,24 @@ var jsrender = global.window.JsViews;
 // export tag registration for users to provide custom tags
 exports.registerTags = jsrender.views.registerTags;
 
+// create a new template which you can invoke directly
+// template(locals);
+exports.compile = function(string) {
+
+    if (!string || typeof string !== "string") {
+        throw new Error('template must be a string');
+    }
+
+    // by using null, jsrender won't cache the object
+    // it will return the template compiled to us
+    var templ = jsrender.template( null, string );
+    return {
+        render: function(locals) {
+            return templ(locals);
+        }
+    };
+}
+
 // express view compiler
 exports.express = {
     compile: function (markup, options) {
